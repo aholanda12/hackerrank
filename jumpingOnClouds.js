d053b7cf69c977afa4727ab8b234429968f46b2c
Ashley Holanda <aholanda12@gmail.com>
2021-03-16 10:50:31 -0400

    jumping on clouds challenge solved

---
+++ jumpingOnClouds.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c) {
+  var minJump = 0;
+  for (let i=0; i < c.length; i++) {
+        if (c[i] === 0) {
+            if (c[i] === c[i+1]) {
+                minJump++
+                i++
+            }
+      }
+      else {
+              minJump++
+          }
+  }
+return minJump;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
